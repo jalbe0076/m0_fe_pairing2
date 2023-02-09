7ec5dfce075d2c327b4786b038ade4d36a795e88
Jason Alberto <alberto.jason@gmail.com>
2023-02-08 19:11:54 -0500

    Add shift push array

--- friends.js
+++ friends.js
@@ -3,3 +3,11 @@ var arrays = ["Harold", "Jeremy", "Danny", "Carter", "Jim"];
 console.log(arrays['0']);
 console.log(arrays['4']);
 console.log(arrays['2']);
+
+var vehicles = ["Car", "Bike", "Motorcycle", "Boat", "Plane"];
+
+var firstElement = vehicles.shift();
+var count = vehicles.push(firstElement);
+
+console.log(vehicles);
+console.log(count);
